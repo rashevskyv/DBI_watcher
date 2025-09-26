543dd47981996590002f1cf7bc4a8a94c8f899c4
rashevskyv <rashevsky.v@gmail.com>
2025-09-26 22:00:27 +0300

    'Update', not 'Download'

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import sys
 import requests
 
 FILE_HEADER = (";LANGUAGES\n\n"
-"[Download list of translations]\n"
+"[Update list of translations]\n"
 "catch_errors\n"
 "download https://github.com/rashevskyv/DBI_watcher/raw/main/output/config.ini '/config/uberhand/downloads/dbi.config.ini'\n"
 "move '/config/uberhand/downloads/dbi.config.ini' '/switch/.packages/Software/DBI/config.ini'\n\n"
